138816c5b40a097e1bdd34860713bf193a2ec182
enesxxa1 <enesxxa1@gmail.com>
2026-02-24 05:01:41 +0300

    Create kullanici.js file with user authentication functions

---
+++ js/kullanici.js
@@ -0,0 +1,21 @@
+// User Authentication Functions
+
+// Function for user registration
+function registerUser(userData) {
+    // Code for registering user goes here
+}
+
+// Function for user login
+function loginUser(credentials) {
+    // Code for user login goes here
+}
+
+// Function for user profile management
+function manageProfile(userId, updatedData) {
+    // Code for managing user profile goes here
+}
+
+// Function for retrieving order history
+function getOrderHistory(userId) {
+    // Code for getting user order history goes here
+}
